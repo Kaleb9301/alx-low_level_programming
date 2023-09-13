b3a830fa95223bbaddcee2617afa0b8bbbaf2646
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-09-13 22:16:35 +0000

    printing the whole lower case alphabet 10X(times0

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * print_alphabet_x10 - printst the the lower case alphabet 10 times
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char j;
+
+	while (i < 10)
+	{
+		j = 'a';
+
+		while (j < 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+
+	}
+
+}
