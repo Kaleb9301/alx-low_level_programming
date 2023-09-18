133a513ff481fcbb0f0498c8454c032e5e3175b9
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-09-18 15:59:45 +0000

    reseting the value of the variable using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - it will reset the value passed by pointer
+ *
+ * @n : this the memory address passed
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
