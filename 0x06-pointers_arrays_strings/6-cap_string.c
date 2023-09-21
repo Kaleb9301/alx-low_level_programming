7fc7185d13447d19aa85cfd49c1ade1362696dff
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-09-21 19:59:39 +0000

    capitalize the fist letters

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *cap_string - captalize the first letter of the world after the speraratros
+ * @s: the stiring
+ * Return: the capitaleized stirng
+ */
+char *cap_string(char *s)
+{
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+				*s -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
