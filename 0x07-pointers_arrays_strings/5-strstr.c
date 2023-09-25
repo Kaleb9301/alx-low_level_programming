35dfdebf5038fb0916ab195feb98f6ce760a2643
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-09-25 18:57:00 +0000

    the sixth task

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - the check and return the result
+ * @haystack: the hay stack
+ * @needle: the needle stack
+ * Return: return the stirng
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	char *str;
+
+	str = "Nothing here";
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[i] != '\0'; j++)
+		{
+			if (haystack[i + `j] == needle[j])
+			{
+				if (needle[j + 1] == '\0')
+					return (needle);
+			}
+		}
+	}
+	return (str);
+}
