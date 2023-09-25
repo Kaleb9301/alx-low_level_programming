c39cfe6770505605987ba76fac5d40eb0971a722
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-09-25 19:56:54 +0000

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,7 +16,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[i] != '\0'; j++)
 		{
-			if (haystack[i + `j] == needle[j])
+			if (haystack[i + j] == needle[j])
 			{
 				if (needle[j + 1] == '\0')
 					return (needle);
