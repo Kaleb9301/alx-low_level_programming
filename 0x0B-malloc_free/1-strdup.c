6726cbf5e10df92caac42740630cccd90475c32c
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-10-03 06:50:32 +0000

    allocatin memory using malloc using it free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdio.h>
 
 /**
  * _strdup - duplicate a string
@@ -10,18 +11,29 @@
 char *_strdup(char *str)
 {
 	char *dup;
-	int i;
+	int i, len;
 
 	if (!str)
 	{
 		return (NULL);
 	}
-	dup = malloc(sizeof(str));
-	for (i = 0; str[i]; i++)
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	dup = malloc((sizeof(char) * len) + 1);
+	if (!dup)
+	{
+		free(dup);
+		free(str);
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
 	{
 		dup[i] = str[i];
 	}
-	dup[i] = '\0';
+	dup[len] = '\0';
 	return (dup);
 
 }
