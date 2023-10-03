cd8411609f0f011541458e6f4caff25c796cf6fb
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-10-03 02:29:25 +0000

    calculate the number of coins in the change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the number of for arg value
+ * @argc: number of argumet
+ * @argv: the argumetn
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, value, coins;
+	int cents[5] = {25, 10, 5, 2, 1};
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	coins = 0;
+	value = atoi(argv[1]);
+	for (i = 0; i < 5; i++)
+	{
+		coins += value / cents[i];
+		value %= cents[i];
+	}
+	printf("%d\n", coins);
+	return (0);
+}
