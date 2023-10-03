b35117b4296cff7666a2633530aa805ecb6c1cc8
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-10-03 14:04:31 +0000

    concatinated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,37 +11,32 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
-	int i, len1, len2;
+	int i, len;
 
-	len1 = 0;
-	while (s1[len1] != '\0')
+	len = 0;
+
+	str = malloc(sizeof(s1) + sizeof(s2) - 4);
+	if (str == NULL)
 	{
-		len1++;
+		return(NULL);
 	}
-	len2 = 0;
-	while (s2[len2] != '\0')
+	if(s1 != NULL)
 	{
-		len2++;
-	}
-	str = malloc(sizeof(char) * (len1 + len2) + 1);
-	if (!str)
-	{
-		return (NULL);
-	}
-	if (s1 != NULL)
-	{
-		for (i = 0; i < len1; i++)
+		while ( s1[len] != '\0')
 		{
-			str[i] = s1[i];
+			str[len] = s1[len];
+			len++;
 		}
 	}
+	i = 0;
 	if (s2 != NULL)
 	{
-		for (i = 0; i < len2; i++)
+		while (s2[i] != '\0')
 		{
-			str[len1 + i] = s2[i];
+			str[len + i] = s2[i];
+			i++;
 		}
 	}
-	str[len1 + len2 + 1] = '\0';
+	str[len + i + 1] = '\0';
 	return (str);
 }
