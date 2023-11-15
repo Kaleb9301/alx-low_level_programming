a9df6f828dc7375c49b62909c9cc1b4e846519fd
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-11-15 21:02:08 +0000

    The advanced or option task1 for recursion project

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,34 @@
 #include "main.h"
-
+/**
+ * _strlen - length of the string
+ * @s: the string to be printed
+ * Return: the length of string
+ */
+int _strlen(char *s)
+{
+	if(s[0] != '\0')
+		return (1 + _strlen(s +1));
+	return(0);
+}
+/**
+ * pal_check - check the string is palindrome
+ * @s: the string address
+ * @i: starter inder
+ * @j: end index
+ * Return: 1 if s is palindrome, 0 otherwise
+ */
+int pal_check(char *s, int i, int j)
+{
+	if (s[i] == s[j])
+	{
+		if ( i < j)
+			return (pal_check(s, i + 1, j - 1));
+		else
+			return (1);
+	}
+	else
+		return (0);
+}
 /**
  * is_palindrome - check wheather the string plaindrome or not
  * @s: the string to be checked
@@ -8,24 +37,5 @@
  */
 int is_palindrome(char *s)
 {
-	int count, start_index, end_index;
-	char *temp;
-
-	temp = s;
-	count = 0;
-	start_index = 0;
-	while (*temp != '\0')
-	{
-		count++;
-		temp++;
-	}
-	end_index = count - 1;
-	while (start_index < end_index)
-	{
-		if (s[start_index] != s[end_index])
-			return (0);
-		start_index++;
-		end_index--;
-	}
-	return (1);
+	return (pal_check(s, 0, _strlen(s) - 1));
 }
