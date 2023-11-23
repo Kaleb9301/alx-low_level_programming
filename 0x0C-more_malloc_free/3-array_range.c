8153702fd68c81d71a9ba1843223f17532ade1ca
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-11-23 13:11:39 +0000

    task 4

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	ptr = malloc((sizeof(int) * max) + 1);
+	ptr = malloc((sizeof(int)) * (max - min + 1));
 	if (ptr == NULL)
 		return (NULL);
 	for (i = min; i < max + 1; i++)
