0ddbae256f9d9e7a13c9fac3d1f532daf84c6444
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-11-27 15:30:35 +0000

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - free up the memory lcoation
+ * @d - the pointer to the struct
+ *
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
