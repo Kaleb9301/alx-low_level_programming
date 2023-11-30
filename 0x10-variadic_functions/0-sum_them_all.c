9576d0e4b43e46a1b2e34779b6bd540eb8525bdf
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-11-30 09:08:52 +0000

    task 1 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,11 +12,12 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	sum = 0;
+	
 	va_start(ap, n);
 
 	if (n == 0)
 		return (0);
+	sum = 0;
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
