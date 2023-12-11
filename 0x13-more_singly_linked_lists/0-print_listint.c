8b8a74063838ace3784bc898fc43c5f8d0200fb8
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-12-11 23:57:02 +0000

    updatad on task 0 code

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,8 +14,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		_putchar(h->n + '0');
-		_putchar('\n');
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef LISTS_H
-#define LISTS_Hi
+#define LISTS_H
 #include <stddef.h>
 /**
  * struct listint_s - singly linked list
@@ -14,7 +14,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 
-}listint_t;
+} listint_t;
 int _putchar (char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
