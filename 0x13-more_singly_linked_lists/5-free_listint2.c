07e6d761b5c64de719de8cd5a0697a831bfc46f3
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-12-12 00:48:17 +0000

    task 4

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - free up the linked list
+ * @head: location of the pointer
+ *
+ * Retrun: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		ptr = (*head)->next;
+		free(*head);
+		*head = ptr;
+	}
+}
