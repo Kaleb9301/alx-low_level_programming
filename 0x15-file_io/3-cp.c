1aa7b93edb6f3c943a47703bef71eaace3070900
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-12-18 22:55:14 +0000

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,7 +40,7 @@ void check98(ssize_t check, char *file, int fd_from, int fd_to)
 }
 
 /**
- * check99 - checs that file_to was creat and/or can be writen to 
+ * check99 - checs that file_to was creat and/or can be writen to
  * @check: checs if true of false
  * @file: file_to name
  * @fd_from: file descriptor of file_from, or -1
@@ -50,10 +50,10 @@ void check98(ssize_t check, char *file, int fd_from, int fd_to)
  */
 
 void check99(ssize_t check, char *file, int fd_from, int fd_to)
-{ 
+{
 	if (check == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",file);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		if (fd_from != -1)
 			close(fd_from);
 		if (fd_to != -1)
@@ -98,7 +98,7 @@ int main(int argc, char *argv[])
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
 	check99((ssize_t)fd_to, argv[2], fd_from, -1);
 	lenr = 1024;
-	while ( lenr == 1024)
+	while (lenr == 1024)
 	{
 		lenr = read(fd_from, buffer, 1024);
 		check98(lenr, argv[1], fd_from, fd_to);
