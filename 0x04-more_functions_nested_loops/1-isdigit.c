c0b74d26a666dbfefc548d55cace789810c463c9
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2024-05-08 06:10:37 +0000

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
- * _isdigit - checke wheater the charter is number or not
- * @c: the character to be checked
- * Return: 1 if it is number 0 otherwise
- */
+*_isdigit - function that verifies if a  character is a digit or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
+
 int _isdigit(int c)
 {
-	if (c >= 45 && c <= 54)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if ((c >= 48) && (c <= 57))
+return (1);
+
+return (0);
 }
+
 
